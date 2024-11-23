6364231478198b2250b665f1ca6154f2f4b9e043
PannnTastic <pannntastic@gmail.com>
2024-11-23 15:05:48 +0700

    app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 // const fertilizerRoutes = require('./routes/');
 const pupuk = require('./routes/pupuk.js');
 const bibit = require('./routes/bibit.js');
+const mysqlBibitRoutes = require('./routes/bibitmysql.js');
+const mysqlpupukRoutes = require('./routes/pupuk-mysql.js');
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use("/bibit", bibit);
 app.use("/pupuk", pupuk);
+app.use('/bibit-mysql', mysqlBibitRoutes);
+app.use('/pupuk-mysql',mysqlpupukRoutes);
 
 app.get('/', (req, res) => {
     res.redirect('/bibit'); // Redirect ke halaman daftar bibit secara default
